579b114137d527850c011cb788b3a10b7bc0d164
Rifyalaidil <rifyalaidil05@gmail.com>
2025-04-03 23:40:58 +0700

    UPDATE DASHBOARD UI(More Flexible..)

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -11,7 +11,7 @@ class Dashboard extends StatefulWidget {
   final List<Book> _books = [
     Book(
       id: '1',
-      title: 'Buku 1: Pulang',
+      title: 'Buku 1',
       author: 'Penulis Buku 1',
       price: 25000,
       imageUrl:
@@ -19,14 +19,14 @@ class Dashboard extends StatefulWidget {
     ),
     Book(
       id: '2',
-      title: 'Buku 2: Angkasa',
+      title: 'Buku 2',
       author: 'Penulis Buku 2',
       price: 30000,
       imageUrl: 'https://cdn.gramedia.com/uploads/items/9786239712716.jpg',
     ),
     Book(
       id: '3',
-      title: 'Buku 3: Wujud',
+      title: 'Buku 3',
       author: 'Penulis Buku 3',
       price: 20000,
       imageUrl:
@@ -37,31 +37,31 @@ class Dashboard extends StatefulWidget {
         title: 'Buku 4',
         author: 'Penulis 4',
         price: 40000,
-        imageUrl: 'https://via.placeholder.com/150/FF0000/FFFFFF?text=Buku4'),
+        imageUrl: 'https://blog.sribu.com/wp-content/uploads/2024/07/214639-20231011-cover_31.jpg-642x1024.webp'),
     Book(
         id: '5',
         title: 'Buku 5',
         author: 'Penulis 5',
         price: 50000,
-        imageUrl: 'https://via.placeholder.com/150/00FF00/FFFFFF?text=Buku5'),
+        imageUrl: 'https://cdn.venngage.com/template/thumbnail/310/9553a2c4-731a-4065-bec9-8b184df4045d.webp'),
     Book(
         id: '6',
         title: 'Buku 6',
         author: 'Penulis 6',
         price: 60000,
-        imageUrl: 'https://via.placeholder.com/150/0000FF/FFFFFF?text=Buku6'),
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSDQnigiVI5kU-UYO_Ourb8H5uaXUP-FYYZ7SFSqMSPTb3ZcCxg2rBwRqcINTKAp7uVNQ&usqp=CAU'),
     Book(
         id: '7',
         title: 'Buku 7',
         author: 'Penulis 7',
         price: 70000,
-        imageUrl: 'https://via.placeholder.com/150/FFFF00/000000?text=Buku7'),
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP0gCZeGK8-rF4puqBoIQgnEAVJNT90RuIkA&s'),
     Book(
         id: '8',
         title: 'Buku 8',
         author: 'Penulis 8',
         price: 80000,
-        imageUrl: 'https://via.placeholder.com/150/FF00FF/FFFFFF?text=Buku8'),
+        imageUrl: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/contemporary-fiction-night-time-book-cover-design-template-1be47835c3058eb42211574e0c4ed8bf_screen.jpg?ts=1734004864'),
   ];
   final List<String> _bannerImages = [
     'https://img.freepik.com/premium-vector/playful-cartoon-book-illustration-with-vector-characters_1323048-20751.jpg',
